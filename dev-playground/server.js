f20c13673aa4ca2c4d3f987b96aa7907ea56e8cf
ASUS\Kai_You <kai_you@asus.com>
2023-05-15 10:14:21 +0800

    add chinese env

--- dev-playground/server.js
+++ dev-playground/server.js
@@ -1,25 +1,59 @@
+var http = require("http");
+var express = require("express");
+var path = require("path");
+var bodyParser = require("body-parser");
 
-var http = require('http');
-var express = require('express');
-var path = require('path');
-var bodyParser = require('body-parser');
-
-var pdfmake = require('../js/index');
+var pdfmake = require("../js/index");
 
 var app = express();
 
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(bodyParser.json({ limit: '50mb' }));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(bodyParser.json({ limit: "50mb" }));
 app.use(bodyParser.urlencoded({ extended: false }));
 
 function createPdfBinary(docDefinition) {
 	var fonts = {
 		Roboto: {
-			normal: path.join(__dirname, '..', 'examples', '/fonts/Roboto-Regular.ttf'),
-			bold: path.join(__dirname, '..', 'examples', '/fonts/Roboto-Medium.ttf'),
-			italics: path.join(__dirname, '..', 'examples', '/fonts/Roboto-Italic.ttf'),
-			bolditalics: path.join(__dirname, '..', 'examples', '/fonts/Roboto-MediumItalic.ttf')
-		}
+			normal: path.join(
+				__dirname,
+				"..",
+				"examples",
+				"/fonts/Roboto-Regular.ttf"
+			),
+			bold: path.join(__dirname, "..", "examples", "/fonts/Roboto-Medium.ttf"),
+			italics: path.join(
+				__dirname,
+				"..",
+				"examples",
+				"/fonts/Roboto-Italic.ttf"
+			),
+			bolditalics: path.join(
+				__dirname,
+				"..",
+				"examples",
+				"/fonts/Roboto-MediumItalic.ttf"
+			),
+		},
+		TaipeiSansTCBeta: {
+			normal: path.join(
+				__dirname,
+				"..",
+				"examples",
+				"/fonts/TaipeiSansTCBeta-Regular.ttf"
+			),
+			bold: path.join(
+				__dirname,
+				"..",
+				"examples",
+				"/fonts/TaipeiSansTCBeta-Bold.ttf"
+			),
+			light: path.join(
+				__dirname,
+				"..",
+				"examples",
+				"/fonts/TaipeiSansTCBeta-Light.ttf"
+			),
+		},
 	};
 
 	pdfmake.setFonts(fonts);
@@ -28,21 +62,23 @@ function createPdfBinary(docDefinition) {
 	return pdf.getDataUrl();
 }
 
-app.post('/pdf', function (req, res) {
-	const dd = new Function(req.body.content + '; return dd;')();
+app.post("/pdf", function (req, res) {
+	const dd = new Function(req.body.content + "; return dd;")();
 
-	createPdfBinary(dd).then(function (binary) {
-		res.contentType('application/pdf');
-		res.send(binary);
-	}, function (error) {
-		res.send('ERROR:' + error);
-	});
-
+	createPdfBinary(dd).then(
+		function (binary) {
+			res.contentType("application/pdf");
+			res.send(binary);
+		},
+		function (error) {
+			res.send("ERROR:" + error);
+		}
+	);
 });
 
 var server = http.createServer(app);
 var port = process.env.PORT || 1234;
 server.listen(port);
 
-console.log('http server listening on port %d', port);
-console.log('dev-playground is available at http://localhost:%d', port);
+console.log("http server listening on port %d", port);
+console.log("dev-playground is available at http://localhost:%d", port);
